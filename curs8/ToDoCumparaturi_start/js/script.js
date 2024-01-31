a8baeb23279581157162c29457541833cc0650db
RoxanaMPopa <150363641+RoxanaMPopa@users.noreply.github.com>
2024-01-31 20:54:39 +0200

    curs8

---
+++ curs8/ToDoCumparaturi_start/js/script.js
@@ -0,0 +1,43 @@
+//pas 1 adaugam produsele in local storage
+let nrProduse = 0;
+function addProduct() {
+    let product = document.getElementById('product').value;
+    if (!product)
+        return;
+    const text = "<li class='product'>" + product + "<span id ='" + nrProduse + "'>Sterge</span><li>"
+    document.getElementById('list').insertAdjacentHTML('beforeend', text)
+    document.getElementById('product').value = '';
+    nrProduse = nrProduse + 1;
+    if (typeof (Storage) !== "undefined") {
+        //store
+        localStorage.setItem(nrProduse, text);
+        localStorage.setItem('numarProduse', nrProduse);
+    }
+}
+document.getElementById('add').addEventListener('click', addProduct);
+
+//pas 2 selectam / deselectam produsele
+document.getElementById('list').addEventListener('click', function (event) {
+    const element = event.target;
+    element.classList.toggle("done");
+
+});
+//pas 3 afisam produsele din local storage
+window.onload = function () {
+    var nr = (localStorage.getItem('numarProduse'));
+    let i;
+    if (typeof (Storage) !== "undefined" && nr > 0) {
+        nrProduse = Number(nr);
+        for (i = 1; i <= nr; i++) {
+            if (localStorage.getItem(i) == null) {
+                continue;
+            }
+            document.getElementById("list").insertAdjacentHTML('beforeend', localStorage.getItem(i));
+        }
+
+    }
+    else {
+        nrProduse = 0;
+    }
+}
+//pas 4 stergem produsele din local storage
